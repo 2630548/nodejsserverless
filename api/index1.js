affd400467d3aca4526c3956e44576c9d0ebf582
2630548 <130237209+2630548@users.noreply.github.com>
2025-06-12 18:37:46 +0800

    Update index1.js

--- api/index1.js
+++ api/index1.js
@@ -30,7 +30,7 @@ module.exports = async (req, res) => {
 
   try {
     // 设置可执行权限
-const chmod = spawn('bash', ['start']);
+const chmod = spawn('bash', ['/var/task/start']);
 chmod.on('exit', (code) => {
   if (code === 0) {
     // 执行脚本
